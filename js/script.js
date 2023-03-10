9b550131c03ce2332b1d5b8d345a77f778a074b3
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-03-10 14:54:21 +0100

    end project

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
+// variabili di appoggio
 let divisionFact = "";
+let boxClass = "";
+// variabili DOM
+const rowElement = document.querySelector(".row");
 
 for (i = 1; i <= 100; i++) {
     if (i % 15 == 0) {
         divisionFact = "FizzBuzz";
+        boxClass = " fizz-buzz";
     } else if (i % 5 == 0) {
         divisionFact = "Buzz";
+        boxClass = " buzz";
+
     } else if (i % 3 == 0) {
         divisionFact = "Fizz";
+        boxClass = " fizz";
     }else{
-        divisionFact = "";
+        divisionFact = `${i}`;
+        boxClass = "";
     }
-    console.log(i, divisionFact);
-
+    console.log(i, divisionFact, boxClass);
+    rowElement.innerHTML += `<div class="col${boxClass}"><span >${divisionFact}</span></div>`;
 }
